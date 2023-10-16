6619bfee8f0efda92db5e5184ca34953ac484c4d
Mikhail Zaitsau <mikhailzaitsau.work@gmail.com>
2023-10-16 16:27:25 +0300

    Add request spec for TDD development

---
+++ spec/requests/flight_routes_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'FlightRoutesController' do
+  describe 'GET /index' do
+    let(:flightnumber) { 'BA2490' }
+    # let(:params) { { flightnumber: } }
+
+    it 'return status OK' do
+      get "/flightroutes/#{flightnumber}"
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'return JSON format' do
+      get "/flightroutes/#{flightnumber}"
+      expect(response.content_type).to eq('application/json')
+    end
+  end
+end
