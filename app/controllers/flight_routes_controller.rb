048a0b65d03beed9d94c065371c63b98e3cb22c8
Mikhail Zaitsau <mikhailzaitsau.work@gmail.com>
2023-10-18 20:27:14 +0300

    Add flight routes controller and first version logic

---
+++ app/controllers/flight_routes_controller.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class FlightRoutesController < ApplicationController
+  include Dry::Monads::Result::Mixin
+
+  def index
+    @flight_number = params[:flight_number]
+
+    if @flight_number.present?
+      fetch_flight_route_by_flight_number
+    else
+      Handlers::ErrorMessageHandler.new.call('Flight number not received', :bad_request)
+    end
+  end
+
+  private
+
+  attr_reader :normalized_flight_number
+
+  def fetch_flight_route_by_flight_number
+    case check_and_normalize_flight_number
+    in Success(normalized_flight_number:)
+      fetch_route_from_api
+    in Failure(error)
+      render json: error
+    end
+  end
+
+  def check_and_normalize_flight_number
+    Handlers::NumberNormalizer.new.call(@flight_number)
+  end
+
+  def fetch_route_from_api
+    Handlers::RoutesFetcher.new.call(normalized_flight_number:)
+  end
+end
